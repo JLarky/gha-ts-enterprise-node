a0ec6e199c8e34e248c299ef59383811d8ea9b4a
Yaroslav Lapin <ylapin@legalzoom.com>
2025-10-30 09:09:52 -0600

    update 0.1.6

--- .github/workflows/hello-world.main.ts
+++ .github/workflows/hello-world.main.ts
@@ -1,27 +1,17 @@
 #!/usr/bin/env -S node --no-warnings
 import { workflow } from "@jlarky/gha-ts/workflow-types";
-import { generateWorkflowYaml } from "./utils/yaml.ts";
-import { lines } from "@jlarky/gha-ts/utils";
 
-import { checkoutStep } from "./utils/steps.ts";
+import { generateWorkflowYaml } from "./utils/yaml.ts";
 
 const wf = workflow({
   name: "Example workflow",
-  on: {
-    push: { branches: ["main"] },
-    pull_request: {},
-  },
+  on: { push: { branches: ["main"] }, pull_request: {} },
   jobs: {
     exampleJob: {
       "runs-on": "ubuntu-latest",
       steps: [
-        checkoutStep({ "fetch-depth": 0 }),
-        {
-          name: "Test",
-          run: lines(`
-            echo 'Hello, world!'
-          `),
-        },
+        { uses: "actions/checkout@v5", with: { "fetch-depth": 0 } },
+        { name: "Test", run: "echo 'Hello, world!'\n" },
       ],
     },
   },
